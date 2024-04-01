25f1504616fe542cd374f8f723774c6935fd2cdb
Martin Dalesio <dalesiomartin@hotmail.com>
2024-04-01 01:27:27 -0300

    Version 1.1 Atributos del Dominio

---
+++ dominio/Articulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+
+       public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        [DisplayName("Descripción")]
+        public int Descripcion { get; set; }
+
+        public int IdMarca { get; set; }
+
+        public int IdCategoria { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public float Precio { get; set; }
+
+
+
+    }
+}
---
+++ dominio/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Descripción")]
+        public int Descripcion { get; set; }
+    }
+}
